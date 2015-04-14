f088ddeb518a5dd4d4437603f6c5d761f6d39c39
mejdi OURIR <m.ourir@ivsweb.com>
2015-04-14 15:03:24 +0200

    typed buffer + fs

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('mootools-more');
 require('./src/mts/Events.Pseudos.js');
 require('./src/natives.js');
 require('./src/node/child_process.js');
-require('./src/node/fs.js');
+require('./src/node/fs.ts');
 require('./src/node/path.js');
 require('./src/node/http.js');
 require('./src/node/util.js');
